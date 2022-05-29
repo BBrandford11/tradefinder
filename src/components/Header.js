89a58a66103b42709cecd26be460ecd4ab35e705
BBrandford11 <brandfordbryce@gmail.com>
2022-05-29 12:58:52 -0600

    clean up react app and added header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="header">
+      <p className="header-words">Jobbing</p>
+      <div className="header-right">
+      <p className="header-words">sign in</p>
+      <p className="header-words">contact us</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
