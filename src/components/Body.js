4f7e582eaccb70810c35bb120c3b7f23c4f0f8e0
BBrandford11 <brandfordbryce@gmail.com>
2022-05-29 15:21:42 -0600

    add picture to main pahge and set up first screen

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,28 @@
+import "./Body.css";
+
 function Body() {
-  return ( <div>body</div> );
+  return (
+    <div className="mainPageBody">
+      <div className="mainPageBody1">
+        <img
+          className="photo1"
+          src="assets/sparkyPhoto.jpg"
+          alt="First slide"
+        />
+      </div>
+      <div className="mainPageBody1-right">
+        <div>
+          <h1>You can count on Jobbing to keep your business organized</h1>
+        </div>
+        <p>
+          {" "}
+          Manage your business and back-and-forth with customers from one place.
+          Estimate, quote, schedule, invoice, get paid—no software experience
+          required.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Body;
