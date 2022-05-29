cfa96d4118aacb58161d520f069792551a431192
BBrandford11 <brandfordbryce@gmail.com>
2022-05-29 16:10:25 -0600

    main page right side

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,15 +11,18 @@ function Body() {
         />
       </div>
       <div className="mainPageBody1-right">
-        <div>
-          <h1>You can count on Jobbing to keep your business organized</h1>
+        <div >
+          <h1 className=" mainPageBody1-right-1">You can count on Jobber to keep your business organized</h1>
+
+
         </div>
-        <p>
-          {" "}
+        <p className=" mainPageBody1-right-2">
           Manage your business and back-and-forth with customers from one place.
           Estimate, quote, schedule, invoice, get paid—no software experience
           required.
         </p>
+        <input className=" mainPageBody1-input" type="email" placeholder="    Email Address" />
+        <p>Try Jobbing - It's free</p>
       </div>
     </div>
   );
