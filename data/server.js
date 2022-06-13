8b3e20309e6f191ebb07dee9702e57ca29a2aec8
BBrandford11 <brandfordbryce@gmail.com>
2022-06-13 15:17:48 -0600

    adds express and start of server

---
+++ data/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = 3002;
+
+// middleware
+// app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
