6d9cef1f436fb844e385b7a6089986bc41b7e40d
BBrandford11 <brandfordbryce@gmail.com>
2022-06-15 15:00:18 -0600

    adds list of jobsites

--- src/components/pages/map/Jobsite.js
+++ src/components/pages/map/Jobsite.js
@@ -4,9 +4,14 @@ function JobSite(props) {
     <div>
       {props.data.map((jobs) => {
         return (
-          <div className="jobsites">
-            {" "}
-            <img className="photo" src={jobs.photo} alt="Italian Trulli" />;
+          <div className="jobsite">
+            <div className="photo">
+              <img className="photo" src={jobs.photo} alt="Italian Trulli" />
+            </div>
+            <div>
+              <h3>{jobs.address}</h3>
+              <p>{jobs.description}</p>
+            </div>
           </div>
         );
       })}
